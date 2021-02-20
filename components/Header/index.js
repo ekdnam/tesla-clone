da37728f18c431248f359050c723ff4f93ee1f88
ekdnam <adeetya.m@gmail.com>
2021-02-20 15:32:40 +0530

    add header

---
+++ components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+import styles from './styles';
+
+const header = () => {
+    return (
+        <View style = {styles.container}>
+            <Image style={styles.logo} source={require('../../assets/images/logo.png')}></Image>
+            <Image style={styles.menu} source={require('../../assets/images/menu.png')}></Image>
+        </View>
+    )
+}
+
+export default header;
