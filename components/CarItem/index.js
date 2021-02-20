9abe644d061f6d08858ea688ae81bb38a47fcafa
ekdnam <adeetya.m@gmail.com>
2021-02-20 14:14:49 +0530

    added custom button component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
 import styles from './styles';
+import StyledButton from '../StyledButton';
 
 const CarItem = () => {
     return (
@@ -12,6 +13,8 @@ const CarItem = () => {
                 <Text style={styles.title}>Model S</Text>
                 <Text style={styles.subtitle}>Starting at $70,000</Text>
             </View>
+            <StyledButton type="primary" text="Custom Order" onPress={() => console.warn("Custom order was pressed")}/>
+            <StyledButton type="secondary" text="Existing Inventory" onPress={() => console.warn("Existing inventory was pressed")}/>
         </View>
     )
 }
